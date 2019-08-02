20b5b403c649e7a1dd45f31717ecdc8840132237
Will Bainton <baintonw@gmail.com>
2019-08-02 11:31:20 -0400

    first commit

---
+++ twitterBot.js
@@ -0,0 +1,48 @@
+// define the dependencies
+const twit = require(‘twit’);
+const config = {
+ consumer_key: process.env.consumer_key,
+ consumer_secret: process.env.consumer_secret,
+ access_token: process.env.access_token,
+ access_token_secret: process.env.access_token_secret
+}
+const Twitter = new twit(config);
+
+let retweet = function() {
+ let params = {
+ q: ‘#thepracticaldev, #coding, #nyc, #developers’,
+ result_type: ‘mixed’,
+ lang: ‘en’
+ }
+
+ // search through all tweets using our params and execute a function:
+Twitter.get(‘search/tweets’, params, function(err, data) {
+ // if there is no error
+ if (!err) {
+ // loop through the first 4 returned tweets
+   for (let i = 0; i < 4; i++) {
+ // iterate through those first four defining a rtId that is equal to the value of each of those tweets’ ids
+     let rtId = data.statuses[i].id_str;
+ // the post action
+     Twitter.post(‘statuses/retweet/:id’, {
+ // setting the id equal to the rtId variable
+       id: rtId
+ // log response and log error
+     }, function(err, response) {
+     if (response) {
+       console.log(‘Successfully retweeted’);
+     }
+     if (err) {
+       console.log(err);
+     }
+   });
+   }
+  }
+  else {
+ // catch all log if the search could not be executed
+   console.log(‘Could not search tweets.’);
+   }
+ });
+}
+retweet();
+setInterval(retweet, 600000);
